92e43b5ea19d5a9027605c5c6f10ca5297e9c0f1
Marcelo Leiva <marceloleivasandoval@gmail.com>
2022-01-27 17:55:21 -0300

    Se agrega metodo call

--- bin/main/ejecucion.groovy
+++ bin/main/ejecucion.groovy
@@ -1,42 +1,43 @@
-/* groovylint-disable-next-line CompileStatic */
-pipeline {
-    agent any
+def call(){
+  pipeline {
+      agent any
 
-  environment {
-    STAGE = ''
-  }
+    environment {
+      STAGE = ''
+    }
 
-  parameters {
-    choice choices: ['gradle', 'maven'], description: 'Indicar herramienta de construcción', name: 'buildTool'
+    parameters {
+      choice choices: ['gradle', 'maven'], description: 'Indicar herramienta de construcción', name: 'buildTool'
 
-  }
+    }
 
-  tools {
-    maven '3.8.4'
-  }
+    tools {
+      maven '3.8.4'
+    }
 
-  stages {
-      stage('Pipeline') {
-        steps {
-          script {
-            if (params.buildTool == 'gradle') {
-          gradle() /* archivo gradle.groovy */
-            } else {
-          maven() /* archivo maven.groovy */
+    stages {
+        stage('Pipeline') {
+          steps {
+            script {
+              if (params.buildTool == 'gradle') {
+            gradle() /* archivo gradle.groovy */
+              } else {
+            maven() /* archivo maven.groovy */
+              }
             }
           }
         }
-      }
-  }
-
-  post {
-    success {
-      slackSend color: 'good', message: '[${env.USER}][${env.JOB_NAME}][${params.buildTool}] Ejecución exitosa.'
     }
 
-    failure {
-      slackSend color: 'danger', message: '[${env.USER}][${env.JOB_NAME}][${params.buildTool}] Ejecución fallida en stage "${STAGE}" build número ${env.BUILD_NUMBER}.'
-      error 'Ejecución fallida en stage ${STAGE}'
+    post {
+      success {
+        slackSend color: 'good', message: '[${env.USER}][${env.JOB_NAME}][${params.buildTool}] Ejecución exitosa.'
+      }
+
+      failure {
+        slackSend color: 'danger', message: '[${env.USER}][${env.JOB_NAME}][${params.buildTool}] Ejecución fallida en stage "${STAGE}" build número ${env.BUILD_NUMBER}.'
+        error 'Ejecución fallida en stage ${STAGE}'
+      }
     }
   }
 }
