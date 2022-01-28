929286e97869b0d38068510bcd4d0cb3cd225904
Marcelo Leiva <marceloleivasandoval@gmail.com>
2022-01-27 21:21:11 -0300

    Se agregan if a step de Maven

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -15,56 +15,70 @@ def call(String[] stages){
 
     stage('Test') {
         STAGE = env.STAGE_NAME
-        sh "mvn clean test -e"
+        if (stages.contains("Test")) {
+            sh "mvn clean test -e"
+        }
     }
 
     stage('Jar') {
         STAGE = env.STAGE_NAME
-        sh "mvn clean package -e"
+        if (stages.contains("Jar")) {
+            sh "mvn clean package -e"
+        }
     }
 
     stage('SonarQube') {
         STAGE = env.STAGE_NAME
-        def scannerHome = tool 'SonarQube Scanner 4.6.2'
-            withSonarQubeEnv('SonarQube local'){
-            sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=sonarqube-token -Dsonar.java.binaries=build"
+        if (stages.contains("SonarQube")) {
+            def scannerHome = tool 'SonarQube Scanner 4.6.2'
+                withSonarQubeEnv('SonarQube local'){
+                sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=sonarqube-token -Dsonar.java.binaries=build"
+            }
         }
     }
 
     stage('Run') {
         STAGE = env.STAGE_NAME
-        sh "nohup mvn spring-boot:run &"
+        if (stages.contains("Run")) {
+            sh "nohup mvn spring-boot:run &"
+        }
     }
 
     stage('Wait') {
         STAGE = env.STAGE_NAME
-        println "Sleep 20 seconds"
-        sleep(time: 20, unit: "SECONDS")
+        if (stages.contains("Wait")) {
+            println "Sleep 20 seconds"
+            sleep(time: 20, unit: "SECONDS")
+        }
     }
 
     stage('Curl') {
         STAGE = env.STAGE_NAME
-        sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        if (stages.contains("Curl")) {
+            sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        }
     }
 
     stage('Nexus') {
         STAGE = env.STAGE_NAME
-        nexusPublisher nexusInstanceId: 'nexus',
-        nexusRepositoryId: 'ejemplo-maven',
-        packages: [
-            [
-                $class: 'MavenPackage',
-                mavenAssetList: [
-                    [classifier: '', extension: '', filePath: "${env.WORKSPACE}/build/DevOpsUsach2020-0.0.1.jar"]
-                ],
-                mavenCoordinate: [
-                    artifactId: 'DevOpsUsach2020',
-                    groupId: 'com.devopsusach2020',
-                    packaging: 'jar',
-                    version: '1.0.0'
+        if (stages.contains("Nexus")) {
+            nexusPublisher nexusInstanceId: 'nexus',
+            nexusRepositoryId: 'ejemplo-maven',
+            packages: [
+                [
+                    $class: 'MavenPackage',
+                    mavenAssetList: [
+                        [classifier: '', extension: '', filePath: "${env.WORKSPACE}/build/DevOpsUsach2020-0.0.1.jar"]
+                    ],
+                    mavenCoordinate: [
+                        artifactId: 'DevOpsUsach2020',
+                        groupId: 'com.devopsusach2020',
+                        packaging: 'jar',
+                        version: '1.0.0'
+                    ]
                 ]
             ]
-        ]
+        }
     }
 
 }
