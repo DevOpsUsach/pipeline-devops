0a11163e6e87dcc7f2af4e79241c3ee30c67ae53
Pablo Morales Salinas <pablo.morales@dafiti.cl>
2022-02-02 15:21:54 -0300

    se agregan IF en ejemplo Gradle

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -16,107 +16,125 @@ println "${env.GIT_BRANCH}"
 if (pipelineType == 'CI'){
     figlet 'Integración Continua'
     stage('build'){
-        figlet 'Build & Test'
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}"
-        sh './gradlew clean build'
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+            figlet 'Build & Test'
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}"
+            sh './gradlew clean build'
+        }
     }
     stage('sonar'){
-        figlet 'SonarQube Analysis'        
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}"  
-        def scannerHome = tool 'sonar-scanner';
-        withSonarQubeEnv('sonarqube-server'){
-        sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=pipeline-devops-labm3-gradle -Dsonar.sources=src -Dsonar.java.binaries=build"
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {        
+            figlet 'SonarQube Analysis'        
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}"  
+            def scannerHome = tool 'sonar-scanner';
+            withSonarQubeEnv('sonarqube-server'){
+                sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=pipeline-devops-labm3-gradle -Dsonar.sources=src -Dsonar.java.binaries=build"
+            }
+        }
+    }        
+    stage('run'){
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {         
+            figlet 'Run Jar'    
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}"    
+            sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash gradlew bootRun &'
+        }
+    }
+    stage('test'){
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {         
+            figlet 'Test'             
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}"    
+            sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        } 
+    }
+    stage('nexusci') {
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {         
+            figlet 'NexusCI'            
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}"    
+            nexusPublisher nexusInstanceId: 'nexus',
+            nexusRepositoryId: 'pipeline-devops-labm3',
+            packages: [
+                        [
+                            $class: 'MavenPackage',
+                            mavenAssetList: [
+                            [classifier: '', extension: '', filePath: "${env.WORKSPACE}/build/libs/DevOpsUsach2020-0.0.1.jar"]
+                            ],
+                            mavenCoordinate: [
+                            artifactId: 'DevOpsUsach2020',
+                            groupId: 'com.devopsusach2020',
+                            packaging: 'jar',
+                            version: '0.0.1'
+                            ]
+                        ]
+                    ]
+        }
+    }   
+} else {
+    figlet 'Delivery Continuo'
+    stage('download'){
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {         
+            figlet 'Download Nexus'            
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}"    
+            sh "curl -X GET -u 'admin:koba' http://localhost:8081/repository/pipeline-devops-labm3/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
+            sh "echo ${env.WORKSPACE}"
+            sh "mv DevOpsUsach2020-0.0.1.jar DevOpsUsach2020-1.0.1.jar"
+            sh "ls -ltr"
+        }
+    }
+    stage('rundown'){
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') { 
+            figlet 'Run Downloaded Jar'            
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}"   
+            sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash gradlew bootRun &'
+        }
+    }
+    stage('rest'){
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') { 
+            figlet 'Rest' 
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}" 
+            sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+        }
+    }
+    stage('nexuscd') {
+        if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {         
+            figlet 'NexusCD' 
+            STAGE = env.STAGE_NAME
+            sh 'env'
+            println "Stage: ${env.STAGE_NAME}"    
+            nexusPublisher nexusInstanceId: 'nexus',
+            nexusRepositoryId: 'pipeline-devops-labm3',
+            packages: [
+                        [
+                            $class: 'MavenPackage',
+                            mavenAssetList: [
+                            [classifier: '', extension: '', filePath: "${env.WORKSPACE}/DevOpsUsach2020-1.0.1.jar"]
+                            ],
+                            mavenCoordinate: [
+                            artifactId: 'DevOpsUsach2020',
+                            groupId: 'com.devopsusach2020',
+                            packaging: 'jar',
+                            version: '1.0.1'
+                            ]
+                        ]
+                    ]
         }
     }    
-    stage('run'){
-        figlet 'Run Jar'    
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}"    
-        sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash gradlew bootRun &'
-    }
-        stage('test'){
-        figlet 'Test'             
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}"    
-        sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-    }
-        stage('nexusci') {
-        figlet 'NexusCI'            
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}"    
-        nexusPublisher nexusInstanceId: 'nexus',
-        nexusRepositoryId: 'pipeline-devops-labm3',
-        packages: [
-                    [
-                        $class: 'MavenPackage',
-                        mavenAssetList: [
-                        [classifier: '', extension: '', filePath: "${env.WORKSPACE}/build/libs/DevOpsUsach2020-0.0.1.jar"]
-                        ],
-                        mavenCoordinate: [
-                        artifactId: 'DevOpsUsach2020',
-                        groupId: 'com.devopsusach2020',
-                        packaging: 'jar',
-                        version: '0.0.1'
-                        ]
-                    ]
-                ]
-    }   
-    } else {
-        figlet 'Delivery Continuo'
-        stage('download'){
-        figlet 'Download Nexus'            
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}"    
-        sh "curl -X GET -u 'admin:koba' http://localhost:8081/repository/pipeline-devops-labm3/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
-        sh "echo ${env.WORKSPACE}"
-        sh "mv DevOpsUsach2020-0.0.1.jar DevOpsUsach2020-1.0.1.jar"
-        sh "ls -ltr"
-        }
-        stage('rundown'){
-        figlet 'Run Downloaded Jar'            
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}"   
-        sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash gradlew bootRun &'
-        }
-        stage('rest'){
-        figlet 'Rest' 
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}" 
-        sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
-        }
-        stage('nexuscd') {
-        figlet 'NexusCD' 
-        STAGE = env.STAGE_NAME
-        sh 'env'
-        println "Stage: ${env.STAGE_NAME}"    
-        nexusPublisher nexusInstanceId: 'nexus',
-        nexusRepositoryId: 'pipeline-devops-labm3',
-        packages: [
-                    [
-                        $class: 'MavenPackage',
-                        mavenAssetList: [
-                        [classifier: '', extension: '', filePath: "${env.WORKSPACE}/DevOpsUsach2020-1.0.1.jar"]
-                        ],
-                        mavenCoordinate: [
-                        artifactId: 'DevOpsUsach2020',
-                        groupId: 'com.devopsusach2020',
-                        packaging: 'jar',
-                        version: '1.0.1'
-                        ]
-                    ]
-                ]
-        }    
-    } 
+} 
 
 }
   
