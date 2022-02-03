4604baf6bbbaec64dfeb4534a085fd95dbcedb61
Juan Hurtado <juan.hurtado@equiliza.com>
2022-02-02 23:21:41 -0300

    Actualizar labboratorio

--- vars/gradle.groovy
+++ vars/gradle.groovy
@@ -18,7 +18,7 @@ def call(){
                     env.STAGE=env.STAGE_NAME
                     def scannerHome = tool 'sonar-scanner';
                     withSonarQubeEnv('sonar-server') {
-                    bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=ejemplo-gradle -Dsonar.sources=src -Dsonar.java.binaries=build"
+                    bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=pipeline-devops -Dsonar.sources=src -Dsonar.java.binaries=build"
                     }
                 }
         }
