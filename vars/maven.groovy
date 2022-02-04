9a269b8284fcd35324a3756da7425d7f87630a20
Equiliza <95260129+Equiliza@users.noreply.github.com>
2022-02-04 15:36:32 -0300

    Corregir env.STAGE = env.STAGE_NAME

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -9,28 +9,28 @@ if (pipelineType == 'CI'){
     stage('compile') {
       if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {      
         figlet 'Compile'
-        STAGE = env.STAGE_NAME
+        env.STAGE = env.STAGE_NAME
         sh './mvnw clean compile -e'
       }
     }
     stage('test') {
       if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
         figlet 'Test'  
-        STAGE = env.STAGE_NAME
+        env.STAGE = env.STAGE_NAME
         sh "./mvnw clean test -e"
       }
     }
     stage('package') {
       if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {      
         figlet 'Package'  
-        STAGE = env.STAGE_NAME        
+        env.STAGE = env.STAGE_NAME
         sh "./mvnw clean package -e"
       }
     }
     stage('sonar') {
       if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
         figlet 'Sonarqube Analisis'
-        STAGE = env.STAGE_NAME        
+        env.STAGE = env.STAGE_NAME
         def scannerHome = tool 'sonar-scanner';
         withSonarQubeEnv('sonarqube-server') { 
           sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=pipeline-devops-labm3-maven -Dsonar.sources=src -Dsonar.java.binaries=."
@@ -40,7 +40,7 @@ if (pipelineType == 'CI'){
     stage('run') {
       if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
         figlet 'Run Jar'
-        STAGE = env.STAGE_NAME        
+        env.STAGE = env.STAGE_NAME
         sh "JENKINS_NODE_COOKIE=dontKillMe nohup bash mvnw spring-boot:run &"
         sleep 10
       }
@@ -48,14 +48,14 @@ if (pipelineType == 'CI'){
     stage('test'){
       if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
         figlet 'Test'
-        STAGE = env.STAGE_NAME        
+        env.STAGE = env.STAGE_NAME
         sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
       }
     }
     stage('nexusci') {
       if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
         figlet 'NexusCI'    
-        STAGE = env.STAGE_NAME        
+        env.STAGE = env.STAGE_NAME
         nexusPublisher nexusInstanceId: 'nexus',
         nexusRepositoryId: 'pipeline-devops-labm3',
         packages: [
@@ -79,7 +79,7 @@ if (pipelineType == 'CI'){
         stage('gitCreateRelease') {
             if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {         
                 figlet env.STAGE_NAME           
-                STAGE = env.STAGE_NAME
+                env.STAGE = env.STAGE_NAME
                 userAborted = false
                 crearRelease = false
                 startMillis = System.currentTimeMillis()
@@ -111,7 +111,7 @@ if (pipelineType == 'CI'){
   stage('download'){
     if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
       figlet 'Download Nexus'  
-      STAGE = env.STAGE_NAME
+      env.STAGE = env.STAGE_NAME
       sh "curl -X GET -u 'admin:koba' http://localhost:8082/repository/pipeline-devops-labm3/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
       sh "echo ${env.WORKSPACE}"      
       sh "ls -ltr"
@@ -120,7 +120,7 @@ if (pipelineType == 'CI'){
   stage('rundown'){
     if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
       figlet 'Run Downloaded Jar'   
-      STAGE = env.STAGE_NAME      
+      env.STAGE = env.STAGE_NAME
       sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash mvnw spring-boot:run &'
       sleep 10
     }
@@ -128,13 +128,13 @@ if (pipelineType == 'CI'){
   stage('rest'){
     if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
       figlet 'Rest'  
-      STAGE = env.STAGE_NAME
+      env.STAGE = env.STAGE_NAME
       sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
     }
   }
 
   stage('gitMergeMain') {
-        STAGE = env.STAGE_NAME
+        env.STAGE = env.STAGE_NAME
         figlet "Stage: ${env.STAGE_NAME}"        
         def git = new helpers.Git()
         git.merge("${env.GIT_LOCAL_BRANCH}", 'main')
@@ -142,14 +142,14 @@ if (pipelineType == 'CI'){
     }
 
     stage('gitMergeDevelop') {
-        STAGE = env.STAGE_NAME
+        env.STAGE = env.STAGE_NAME
         figlet "Stage: ${env.STAGE_NAME}"        
         def git = new helpers.Git()
         git.merge("${env.GIT_LOCAL_BRANCH}", 'develop')
     }
 
     stage('gitTagMaster') {
-        STAGE = env.STAGE_NAME
+        env.STAGE = env.STAGE_NAME
         figlet "Stage: ${env.STAGE_NAME}"        
         def git = new helpers.Git()
         git.tag("${env.GIT_LOCAL_BRANCH}",'main')
@@ -157,7 +157,7 @@ if (pipelineType == 'CI'){
   /*stage('nexuscd') {
     if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
       figlet 'NexusCD'    
-      STAGE = env.STAGE_NAME
+      env.STAGE = env.STAGE_NAME
       sh 'env'
       println "Stage: ${env.STAGE_NAME}"    
       nexusPublisher nexusInstanceId: 'nexus',
