6ba6fba9570b06427e6cdf451e85368043c7eca2
Juan Hurtado <juan.hurtado@equiliza.com>
2022-02-04 03:22:00 -0300

    Actualizar laboratorio

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -99,7 +99,7 @@ if (pipelineType == 'CI'){
                     packages: [[$class: 'MavenPackage',
                         mavenAssetList: [[classifier: '',
                         extension: '',
-                        filePath: "${env.WORKSPACE}/DevOpsUsach2020-0.0.1.jar"]],
+                        filePath: "${env.WORKSPACE}/build/DevOpsUsach2020-0.0.1.jar"]],
                         mavenCoordinate: [artifactId: 'DevOpsUsach2020',
                         groupId: 'com.devopsusach2020',
                         packaging: 'jar',
