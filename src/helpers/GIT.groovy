77127700fc68c1353dac007f59d1235d23303116
Juan Hurtado <juan.hurtado@equiliza.com>
2022-02-04 12:15:23 -0300

    Actualizar laboratorio

---
+++ src/helpers/GIT.groovy
@@ -0,0 +1,13 @@
+package helpers
+
+def merge(String ramaOrigen, String ramaDestino){
+	println "Este método realiza un merge ${ramaOrigen} y ${ramaDestino}"
+	
+}
+
+def tag(String ramaOrigen, String ramaDestino){
+	println "Este método realiza un tag ${ramaOrigen} y ${ramaDestino}"
+
+}
+
+return this;
