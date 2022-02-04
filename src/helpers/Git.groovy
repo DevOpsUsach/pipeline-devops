83a5ee7205ed95f0b336715ca607305a96d11f70
Juan Hurtado <juan.hurtado@equiliza.com>
2022-02-04 18:14:13 -0300

    Actualizar laboratorio

--- src/helpers/Git.groovy
+++ src/helpers/Git.groovy
@@ -32,7 +32,7 @@ def tag(String ramaOrigen, String ramaDestino){
 def checkout(String rama){
 	println "Checkout: ${rama}"
 
-	//bat "git reset --hard HEAD"
+	bat "git reset --hard HEAD"
 	bat "git checkout ${rama}"
 	bat "git pull origin ${rama}"
 }
