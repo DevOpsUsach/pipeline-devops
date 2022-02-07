00ad31b28b93177506f6f8b2ec7a50f63ec99abf
Marcelo Leiva <marceloleivasandoval@gmail.com>
2022-02-06 22:17:54 -0300

    Se agrega helper para optener nueva version

---
+++ bin/main/ejecucion.groovy
@@ -0,0 +1,92 @@
+/*
+        forma de invocación de método call:
+        def ejecucion = load 'script.groovy'
+        ejecucion.call()
+*/
+
+def call(){
+
+        pipeline {
+        agent any
+
+                parameters {
+                        choice choices: ['gradle', 'maven'], description: 'Indicar Herramienta de Construcción', name: 'buildTool'
+                        string(name: 'stage', defaultValue: '')
+                }
+
+                stages {
+                        stage('PipeLine') {
+                                steps {
+                                        script {
+                                                println checkOs()
+                                                if (params.stage.length() == 0) {
+                                                        println "ALL"
+                                                        env.STAGE = "NULL"
+                                                        env.PSTAGE = "ALL"
+                                                        if (params.buildTool == "gradle") { gradle(verifyBranchName()) } else { maven(verifyBranchName()) }
+                                                } else {
+                                                        println "Selectivo"
+                                                        def stages = params.stage.split(";")
+                                                        for (i=0; i < stages.size(); i++) {
+                                                                env.STAGE = "NULL"
+                                                                env.PSTAGE = stages[i]
+                                                                if (params.buildTool == "gradle") { gradle(verifyBranchName()) } else { maven(verifyBranchName()) }
+                                                                if (env.STAGE == 'NULL') { break }
+                                                        }
+                                                }
+                                        }
+                                }
+                        }
+                }
+
+                post {
+                        success {
+                                script {
+                                        if (env.STAGE == 'NULL' && env.PSTAGE != 'ALL') {
+                                                slackSend color: 'danger', message: "[Grupo 3][Pipeline: ${env.JOB_NAME}][Branch: ${env.GIT_LOCAL_BRANCH}][Tools: ${params.buildTool}][Stage: ${env.PSTAGE}][Resultado: NOK]"
+                                                error "Ejecución fallida en stage ${env.PSTAGE}"
+                                        } else {
+                                                slackSend color: 'good', message: "[Grupo 3][${env.JOB_NAME}][Branch: ${env.GIT_LOCAL_BRANCH}][${params.buildTool}][Resultado: OK]"
+                                        }
+                                }
+
+                        }
+
+                        failure {
+                                script {
+                                        if (env.STAGE != 'NULL' || env.PSTAGE == 'ALL') {
+                                                slackSend color: 'danger', message: "[Grupo 3][Pipeline: ${env.JOB_NAME}][Branch: ${env.GIT_LOCAL_BRANCH}][Tools: ${params.buildTool}][Stage: ${env.STAGE}][Resultado: NOK]"
+                                                error "Ejecución fallida en stage ${env.STAGE}"
+                                        }
+                                }
+                        }
+                }
+        }
+
+}
+
+def verifyBranchName(){
+    if (env.GIT_BRANCH.contains('develop') || env.GIT_BRANCH.contains('feature-')) {
+        return 'CI'
+    } else {
+        return 'CD'
+    }
+}
+
+def checkOs(){
+    if (isUnix()) {
+        def uname = sh script: 'uname', returnStdout: true
+        if (uname.startsWith("Darwin")) {
+            return "Macos"
+        }
+        // Optionally add 'else if' for other Unix OS
+        else {
+            return "Linux"
+        }
+    }
+    else {
+        return "Windows"
+    }
+}
+
+return this;
---
+++ bin/main/maven.groovy
@@ -0,0 +1,185 @@
+import helpers.*
+def call(String pipelineType){
+
+figlet pipelineType
+figlet 'Maven'  
+
+if (pipelineType == 'CI'){
+    figlet 'Integración Continua'
+    stage('compile') {
+      if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {      
+        figlet 'Compile'
+        env.STAGE = env.STAGE_NAME
+        sh './mvnw clean compile -e'
+      }
+    }
+    stage('test') {
+      if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+        figlet 'Test'  
+        env.STAGE = env.STAGE_NAME
+        sh "./mvnw clean test -e"
+      }
+    }
+    stage('package') {
+      if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {      
+        figlet 'Package'  
+        env.STAGE = env.STAGE_NAME
+        sh "./mvnw clean package -e"
+      }
+    }
+    stage('sonar') {
+      if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+        figlet 'Sonarqube Analisis'
+        env.STAGE = env.STAGE_NAME
+        def scannerHome = tool 'sonar-scanner';
+        withSonarQubeEnv('sonarqube-server') { 
+          sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=pipeline-devops-labm3-maven -Dsonar.sources=src -Dsonar.java.binaries=."
+        }
+      }
+    }
+    stage('run') {
+      if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+        figlet 'Run Jar'
+        env.STAGE = env.STAGE_NAME
+        sh "JENKINS_NODE_COOKIE=dontKillMe nohup bash mvnw spring-boot:run &"
+        sleep 10
+      }
+    }
+    stage('test'){
+      if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+        figlet 'Test'
+        env.STAGE = env.STAGE_NAME
+        sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+      }
+    }
+    stage('nexusci') {
+      if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+        figlet 'NexusCI'    
+        env.STAGE = env.STAGE_NAME
+        nexusPublisher nexusInstanceId: 'nexus',
+        nexusRepositoryId: 'pipeline-devops-labm3',
+        packages: [
+                    [
+                        $class: 'MavenPackage',
+                        mavenAssetList: [
+                        [classifier: '', extension: '', filePath: "${env.WORKSPACE}/build/DevOpsUsach2020-0.0.1.jar"]
+                        ],
+                        mavenCoordinate: [
+                        artifactId: 'DevOpsUsach2020',
+                        groupId: 'com.devopsusach2020',
+                        packaging: 'jar',
+                        version: '0.0.1'
+                        ]
+                    ]
+                ]
+      }
+    }
+
+    if ("${env.GIT_BRANCH}" == "develop"){
+        stage('gitCreateRelease') {
+            if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {         
+                figlet env.STAGE_NAME           
+                env.STAGE = env.STAGE_NAME
+                userAborted = false
+                crearRelease = false
+                startMillis = System.currentTimeMillis()
+                timeoutMillis = 10000
+                
+                try {
+                  timeout(time: timeoutMillis, unit: 'MILLISECONDS') {
+                    input '¿Desea crear un nuevo release?'
+                  }
+                } catch (org.jenkinsci.plugins.workflow.steps.FlowInterruptedException e) {
+                    crearRelease = true
+                }
+                
+                if (crearRelease) {
+                  //currentBuild.result = 'ABORTED'
+                  println "No se desea hacer un release. El pipeline debería continuar"
+                } else {
+                  //currentBuild.result = 'SUCCESS'
+                  println "Pendiente: Vamos a proceder a crear un release"
+                }
+
+            }
+        }
+    }
+
+
+} else {
+  figlet 'Delivery Continuo'
+  stage('download'){
+    if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+      figlet 'Download Nexus'  
+      env.STAGE = env.STAGE_NAME
+      sh "curl -X GET -u 'admin:koba' http://localhost:8082/repository/pipeline-devops-labm3/com/devopsusach2020/DevOpsUsach2020/0.0.1/DevOpsUsach2020-0.0.1.jar -O"
+      sh "echo ${env.WORKSPACE}"      
+      sh "ls -ltr"
+    }
+  }
+  stage('rundown'){
+    if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+      figlet 'Run Downloaded Jar'   
+      env.STAGE = env.STAGE_NAME
+      sh 'JENKINS_NODE_COOKIE=dontKillMe nohup bash mvnw spring-boot:run &'
+      sleep 10
+    }
+  }
+  stage('rest'){
+    if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+      figlet 'Rest'  
+      env.STAGE = env.STAGE_NAME
+      sh "curl -X GET 'http://localhost:8081/rest/mscovid/test?msg=testing'"
+    }
+  }
+
+  stage('gitMergeMain') {
+        env.STAGE = env.STAGE_NAME
+        figlet "Stage: ${env.STAGE_NAME}"        
+        def git = new helpers.Git()
+        git.merge("${env.GIT_LOCAL_BRANCH}", 'main')
+
+    }
+
+    stage('gitMergeDevelop') {
+        env.STAGE = env.STAGE_NAME
+        figlet "Stage: ${env.STAGE_NAME}"        
+        def git = new helpers.Git()
+        git.merge("${env.GIT_LOCAL_BRANCH}", 'develop')
+    }
+
+    stage('gitTagMaster') {
+        env.STAGE = env.STAGE_NAME
+        figlet "Stage: ${env.STAGE_NAME}"        
+        def git = new helpers.Git()
+        git.tag("${env.GIT_LOCAL_BRANCH}",'main')
+    }
+  /*stage('nexuscd') {
+    if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
+      figlet 'NexusCD'    
+      env.STAGE = env.STAGE_NAME
+      sh 'env'
+      println "Stage: ${env.STAGE_NAME}"    
+      nexusPublisher nexusInstanceId: 'nexus',
+      nexusRepositoryId: 'pipeline-devops-labm3',
+      packages: [
+                  [
+                      $class: 'MavenPackage',
+                      mavenAssetList: [
+                      [classifier: '', extension: '', filePath: "${env.WORKSPACE}/DevOpsUsach2020-1.0.2.jar"]
+                      ],
+                      mavenCoordinate: [
+                      artifactId: 'DevOpsUsach2020',
+                      groupId: 'com.devopsusach2020',
+                      packaging: 'jar',
+                      version: '1.0.2'
+                      ]
+                  ]
+              ]
+    }
+  }*/
+}
+
+}
+
+return this;
