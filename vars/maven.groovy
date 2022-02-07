d86f4e9a9cd78d18350091ec85e0786141c34059
Juan Hurtado <juan.hurtado@equiliza.com>
2022-02-07 09:35:13 -0300

    Actualizar laboratorio

--- vars/maven.groovy
+++ vars/maven.groovy
@@ -15,6 +15,8 @@ if (pipelineType == 'CI'){
                     env.STAGE=env.STAGE_NAME
 		    if (checkOs()=="windows") {
                     	bat "./mvnw.cmd clean compile -e"
+		    } else {
+		    	sh "./mvnw clean compile -e"
 		    }
                 }
         }
@@ -24,6 +26,8 @@ if (pipelineType == 'CI'){
                     env.STAGE=env.STAGE_NAME
 		    if (checkOs()=="windows") {
                     	bat "./mvnw.cmd clean test -e"
+		    } else {
+		    	sh "./mvnw clean test -e"
 		    }
                 }
         }
@@ -33,6 +37,8 @@ if (pipelineType == 'CI'){
                     env.STAGE=env.STAGE_NAME
 		    if (checkOs()=="windows") {
                     	bat "./mvnw.cmd clean package -e"
+		    } else {
+		    	sh "./mvnw clean package -e"
 		    }
                 }
         }
@@ -40,9 +46,13 @@ if (pipelineType == 'CI'){
                 if (env.PSTAGE == env.STAGE_NAME || env.PSTAGE == 'ALL') {
 		    figlet "Stage: ${env.STAGE_NAME}"
                     env.STAGE=env.STAGE_NAME
-                    def scannerHome = tool 'scaner-devops';
-                    withSonarQubeEnv('sonar-devops') {
-                    bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=pipeline-devops -Dsonar.sources=src -Dsonar.java.binaries=build"
+                    def scannerHome = tool 'sonar-scaner';
+                    withSonarQubeEnv('sonarqube-server') {
+		    if (checkOs()=="windows") {
+                    	bat "C:/Users/Patric~1/.jenkins/tools/hudson.plugins.sonar.SonarRunnerInstallation/sonar-scanner/bin/sonar-scanner.bat -Dsonar.projectKey=pipeline-devops -Dsonar.sources=src -Dsonar.java.binaries=build"
+		    } else {
+		    	sh "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=pipeline-devops-labm3-maven -Dsonar.sources=src -Dsonar.java.binaries=build"
+		    }
                     }
                 }
         }
